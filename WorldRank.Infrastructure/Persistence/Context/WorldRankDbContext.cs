22b533ad82f9f5d0bcf0815a785d17e87b071c85
Marios <mariossstefanis@gmail.com>
2026-07-12 22:15:21 +0300

    Replace with Day 4 teacher solution

---
+++ WorldRank.Infrastructure/Persistence/Context/WorldRankDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorldRank.Infrastructure.Persistence.Context
+{
+    internal class WorldRankDbContext
+    {
+    }
+}
